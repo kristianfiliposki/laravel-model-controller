890c014d54f5e5c323d28d7b4b276272660686e7
kristianfiliposki <krisplay@outlook.it>
2024-01-25 10:52:35 +0100

    i add var_dump

--- laravel/app/Http/Controllers/MoviesController.php
+++ laravel/app/Http/Controllers/MoviesController.php
@@ -5,14 +5,14 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Movie;
 
+
 class MoviesController extends Controller
 {
     public function index(){
             
-            $dati=config("data");
             $movie=Movie::all();
 
-            return view('serie',compact($dati,$movie));
+            return view('serie',compact("movie"));
 
     }
 }
